7e005401f3a7447a6bc4e89143166ff390aed255
cagriinan46 <xcagriinan46@gmail.com>
2024-03-06 17:11:06 +0300

    main.cpp'ye şifreleme eklendi.
    Şifreleme alt fonksiyonu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
-int main()
+string Sifreleme(string metin)
 {
-  cout<<"Deneme!";  
+    char harf;
+    string sifreliMetin;
+    string polybiusKaresi[5] = {
+        "ABCDE",
+        "FGHIK",
+        "LMNOP",
+        "QRSTU",
+        "VWXYZ"
+    };
+    for(char harf : metin)
+        harf = toupper(harf);
+        if(isalpha(harf))
+        {
+            if(harf == 'J')
+                harf = 'I';
+
+            for(int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if(polybiusKaresi[i][j] == harf)
+                    {
+                        sifreliMetin = sifreliMetin + to_string(i+1);
+                        sifreliMetin = sifreliMetin + to_string(j+1);
+                        sifreliMetin = sifreliMetin + " ";
+                    }
+                }
+            }
+        }   else if (harf == ' ')
+            {
+                sifreliMetin = sifreliMetin + " ";
+            }
+    return sifreliMetin;
 }
